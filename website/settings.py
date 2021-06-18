4acfb2d0da4a1550bdfd9c495a0ca00b13140f9c
Philippe Morel <philippe.a.b.morel@gmail.com>
2021-06-18 12:43:25 -0700

    debug

--- website/settings.py
+++ website/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.getenv('APP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
